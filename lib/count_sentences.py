f8cb21a4e04b7cfd99f0f53d8eaf458fad14b642
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-27 22:41:22 +0300

    endswith a period

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -3,6 +3,13 @@
 class MyString:
     def __init__(self, value=''):
         if not isinstance(value, str):
-            raise ValueError("Value must be a string")
+            print("The value must be a string.")
         self.value = value
+    
+    def is_sentence(self):
+        if self.value.endswith('.'):
+            return True
+        else:
+            return False
 
+        
